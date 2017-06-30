9326378d0baca4e4b94b542100e7f9e9f7611c2a
github username <github email address>
2017-06-30 10:59:17 -0400

    initial commit of app

---
+++ config/environment.rb
@@ -0,0 +1,15 @@
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+configure :development do
+  set :database, 'sqlite3:db/post_it.db'
+end
+
+require_all './app'
+
+
+binding.pry
+
+puts "blah"
