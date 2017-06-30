d8f955a9c9711bf77ae671798f68fa551d340d2e
github username <github email address>
2017-06-30 17:05:23 -0400

    added stuff

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,14 +1,58 @@
 class BlogController < ApplicationController
 
+  get '/blogs/new' do
+    erb :'/blogs/new'
+  end
+
   get '/blogs' do
     @blogs = Blog.all
     erb :'/blogs/index'
   end
-
+  # put this line back in index.erb
+  #<p> <a href="/users/<%= blog.user.id %>" style="color: black">Posted by: <%= blog.user.user_name %></a></p>
   get '/blogs/:id' do
     @blog = Blog.find(params[:id])
     erb :'/blogs/show'
   end
 
+
+  post '/blogs' do
+     # User ID of blog params[:user_id].keys[0]
+     @blog = Blog.create(params[:blog])
+     @user = User.find(params[:user_id].keys[0])
+     @user.blogs << @blog
+     @blog.user = @user
+     #Send back to owner of blogs show page
+     redirect :"/users/#{@user.id}"
+  end
+
+  post '/blogs/:id/comment' do
+    ##Post route here creates comment on blog
+    @blog = Blog.find(params[:id])
+    @comment = Comment.create(params[:comment])
+    @blog.comments << @comment
+    @user = User.last
+    @user.comments << @comment
+    redirect :"/blogs/#{@blog.id}"
+  end
+
+  get '/blogs/:id/edit' do
+    @blog = Blog.find(params[:id])
+    erb :'/blogs/edit'
+  end
+
+  patch '/blogs/:id' do
+    @blog = Blog.find(params[:id])
+    @blog.update!(params[:blog])
+    redirect :"/blogs/#{@blog.id}"
+
+  end
+
+  delete '/blogs/:id' do
+    @blog = Blog.find(params[:id])
+    user_id = @blog.user.id
+    @blog.delete
+    redirect :"/users/#{user_id}"
+  end
 
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,7 @@
 require './config/environment'
 
+
+use Rack::MethodOverride
 use BlogController
 use UserController
 run ApplicationController
