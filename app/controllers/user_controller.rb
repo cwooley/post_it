b5ce71e171af058a7de990844c7e0950c49c29e2
github username <github email address>
2017-06-30 21:17:25 -0400

    Added user/new page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,14 +1,26 @@
 class UserController < ApplicationController
 
+  get '/users/new' do
+    erb :'/users/new'
+  end
+
+  post '/users/new' do
+    @user = User.create(params[user])
+    redirect "/user/#{@user.id}"
+  end
+
   get '/users' do
     erb :'/users/index'
   end
+
 
   get '/users/:id' do
     @user = User.find(params[:id])
     erb :'/users/show'
   end
 
+
   #implement edit action for users
+
 
 end
