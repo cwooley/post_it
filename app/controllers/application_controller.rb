1bda942f503a55475e834ca4c81f9117560c4789
github username <github email address>
2017-07-02 09:22:00 -0400

    built out directories for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,5 @@ class ApplicationController < Sinatra::Base
 
 
 
+
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,4 +11,14 @@ class UserController < ApplicationController
 
   #implement edit action for users
 
+
+  ##Putting authorization routes here for now
+  ## Perhaps they should ahve their own controller
+
+  get '/resgistrations/signup' do
+    erb :signup
+  end
+
+
+
 end
